dfcc1668668dd23967eef33193a1d2198db52884
Norpatin <nooravep@gmail.com>
2025-03-26 23:26:05 +0200

    Versio 1

---
+++ app/src/main/java/com/example/viikko11/Contact.java
@@ -0,0 +1,38 @@
+package com.example.viikko11;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String number;
+    private String contactGroup;
+
+    public Contact(String firstName, String surName, String number, String contactGroup) {
+        this.firstName = firstName;
+        this.lastName = surName;
+        this.number = number;
+        this.contactGroup = contactGroup;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getContactGroup() {
+        return contactGroup;
+    }
+
+    public String getFullName() {  //HUOM!!! tehtävän annossa ei String eikä void
+        String FullName = firstName + " " + lastName;
+        return FullName;
+    }
+
+}
---
+++ app/src/main/java/com/example/viikko11/ContactStorage.java
@@ -0,0 +1,34 @@
+package com.example.viikko11;
+
+import java.util.ArrayList;
+
+public class ContactStorage {
+    private ArrayList<Contact> contacts = new ArrayList<>();
+    private static ContactStorage contactStorage = null;
+
+
+    public static ContactStorage getInstance() {
+        if(contactStorage == null){
+            contactStorage = new ContactStorage();
+        }
+        return contactStorage;
+    }
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void addContact(Contact contact) {
+
+    }
+    public void removeContact(int puhNumber) {
+     int i = 0;
+     String puhNumberString = String.valueOf(puhNumber);
+     for(Contact r : contacts) {
+         if(r.getNumber().equals(puhNumberString)){
+             break;
+         }
+         i++;
+     }
+     contacts.remove(i);
+    }
+}
